4b176801c997ac338343d7dbe640cbfab362e35a
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-08-25 23:37:08 +0530

    Create A2.java

---
+++ Inheritence/A2.java
@@ -0,0 +1,12 @@
+package inheritence_2;
+
+/**
+ * Class A2 - Simple class for upcasting and downcasting demonstration
+ */
+class A2 {
+    int i;
+
+    public void m1() {
+        System.out.println("Method m1 in A2");
+    }
+}
