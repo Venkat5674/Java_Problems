06e2b8f56e5afce2eb7939a348dee7fa5a62a1f3
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-08-25 23:34:04 +0530

    Add Test1 class for constructor chaining demonstration
    This class demonstrates constructor chaining in inheritance by creating an object of class B.

---
+++ Inheritence/ConstructorTest1.java
@@ -0,0 +1,11 @@
+package inheritence_2;
+
+/**
+ * Test1 - Demonstrates constructor chaining in inheritance
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        new B();  // Creates object of B
+        System.out.println();
+    }
+}
