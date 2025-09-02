716fc987dcf0226deb5a139b956d3cb11b326c85
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-09-02 12:20:42 +0530

    Add Test1 class demonstrating abstraction usage

---
+++ Abstraction/Test1.java
@@ -0,0 +1,11 @@
+package abstraction;
+
+public class Test1 {
+    public static void main(String[] args) {
+        // Use child class object instead of abstract class
+        A1 obj = new B1();
+        obj.m1();
+        obj.m2();
+        obj.m3(); // concrete method
+    }
+}
