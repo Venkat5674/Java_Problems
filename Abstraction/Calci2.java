b93dcf237b67d0b863a6a1a18cf2d58c51de8a86
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-09-02 12:24:20 +0530

    Create Calci2.java

---
+++ Abstraction/Calci2.java
@@ -0,0 +1,13 @@
+package abstraction;
+
+// 100% abstraction using interface
+public interface Calci2 {
+
+    void add(int a, int b);
+
+    void sub(int a, int b);
+
+    void mult(int a, int b);
+
+    void div(int a, int b);
+}
