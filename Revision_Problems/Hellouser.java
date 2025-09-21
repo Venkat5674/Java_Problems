57c18408cc6eea290cad3df9b5a2d1fc87fd3bed
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-09-21 11:35:21 +0530

    Enhance Hellouser.java with explanatory comments

--- Revision_Problems/Hellouser.java
+++ Revision_Problems/Hellouser.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;  
+// This imports the Scanner class from java.util package.
+// Scanner is used to take input from the user.
 
+class Hellouser {  
+    // This defines a class named 'Hellouser'.
+    // Every Java program must have at least one class.
+
+    public static void main(String[] args) {  
+        // This is the main method.
+        // The JVM starts executing the program from this method.
+
+        Scanner sc = new Scanner(System.in);  
+        // Create a Scanner object named 'sc' to read input from the console.
+
+        String user = sc.nextLine();  
+        // Reads a full line of text entered by the user and stores it in the variable 'user'.
+
+        System.out.println("Hello," + user + " !");  
+        // Prints the message "Hello, [user] !" to the console.
+        // The '+' operator is used to concatenate strings.
+    }
+}
