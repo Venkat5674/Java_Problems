e76a4ed82c35173bdfef37ea4c27f736e3b8148e
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-09-22 22:15:50 +0530

    Add non-static inner class example in NSOuter.java

---
+++ NSOuter.java
@@ -0,0 +1,24 @@
+//Non-static Inner Class (Member Inner Class)
+
+//Defined inside another class without the static keyword.
+
+//Can access all members (even private) of the outer class.
+
+//Needs an object of the outer class to create its object.
+
+class Outer {
+	private String message = "Hello from Outer!";
+ 
+ 	class Inner{
+ 		void display() {
+ 			System.out.println(message);
+ 		}
+ 	}
+}
+public class Main{
+	public static void main(String [] args) {
+		Outer outer = new Outer();
+		Outer.Inner inner = outer.new Inner();
+		inner.display();
+	}
+}
