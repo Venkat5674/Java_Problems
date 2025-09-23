2b19a539a20c8617c75e307e1862e26de3c28c5e
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-09-23 23:26:21 +0530

    Create ReverseString.java

---
+++ Strings/ReverseString.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class ReverseString{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+    
+        String a = sc.nextLine();
+        String b = "";
+        
+        int ptr = a.length()-1;
+        while(ptr >= 0){
+            b = b+a.charAt(ptr);
+            ptr--;
+        }
+        System.out.println(b);
+    }
+}
