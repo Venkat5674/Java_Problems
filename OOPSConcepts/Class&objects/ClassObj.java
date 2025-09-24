135f747343da96958629a2b8edb8fcbca1584237
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-09-24 22:57:05 +0530

    Create ClassObj.java

---
+++ OOPSConcepts/Class&objects/ClassObj.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Student {
+    String name;
+    int age;
+
+    // Constructor
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Method to display details
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+
+        Student s = new Student(name, age);
+        s.display();
+    }
+}
