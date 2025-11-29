968b2264fbd676cd71dd2e1848c85a221493b216
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-11-29 23:04:01 +0530

    Create duplicate.java

---
+++ ArrayList/duplicate.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Example3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(10,20,10,30,20,40,30));
+
+        ArrayList<Integer> unique = new ArrayList<>();
+
+        for(Integer n : list){
+            if(!unique.contains(n)){
+                unique.add(n);
+            }
+        }
+
+        System.out.println("Original List: " + list);
+        System.out.println("After removing duplicates: " + unique);
+    }
+}
