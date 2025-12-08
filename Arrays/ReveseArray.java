97ff1e3a8722068b74589433dadb119de7ba3132
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-12-08 20:52:51 +0530

    Create ReveseArray.java

---
+++ Arrays/ReveseArray.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class ReveseArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+    
+        
+        int i = 0, j = n - 1;
+        while(i < j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        System.out.print("Reversed array: ");
+        for(int x : arr){
+            System.out.print(x + " ");
+        }
+    }
+}
