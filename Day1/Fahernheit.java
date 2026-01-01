5442ce6e165a52e235a944979f37f96d4928eaba
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2026-01-01 10:51:20 +0530

    Add Fahrenheit conversion program
    This program converts Celsius to Fahrenheit and prints both values.

---
+++ Day1/Fahernheit.java
@@ -0,0 +1,11 @@
+class Fahrenheit {
+
+    public static void main(String[] args) {
+
+        int celsius = 27;
+        int fahrenheit = (celsius * 9 / 5) + 32;
+
+        System.out.println(celsius);
+        System.out.println(fahrenheit);
+    }
+}
