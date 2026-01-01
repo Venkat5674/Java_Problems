1d31593895fa6591684bff680335224b1321a2da
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2026-01-01 10:40:17 +0530

    Create Variables.java

---
+++ Day1/Variables.java
@@ -0,0 +1,13 @@
+class Variables {
+
+    public static void main(String[] args) {
+
+        double salary = 25000.00;
+        char letter = 'A';
+        boolean morning = true;
+
+        System.out.println(salary);
+        System.out.println(letter);
+        System.out.println(morning);
+    }
+}
