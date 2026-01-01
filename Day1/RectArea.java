2ca020c54dd625f9c38c3b4d05991440b8e4ba15
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2026-01-01 10:44:28 +0530

    Create RectArea.java

---
+++ Day1/RectArea.java
@@ -0,0 +1,11 @@
+class RectArea {
+
+    public static void main(String[] args) {
+
+        int l = 4;
+        int b = 5;
+        double area = (double) l * b;
+
+        System.out.println(area);
+    }
+}
