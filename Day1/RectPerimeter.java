05fcbe80b5dac087f3e0cd8839144c754aac97d2
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2026-01-01 10:46:18 +0530

    Add RectPerimeter class to calculate rectangle perimeter
    Implement a Java program to calculate the perimeter of a rectangle.

---
+++ Day1/RectPerimeter.java
@@ -0,0 +1,11 @@
+class RectPerimeter {
+
+    public static void main(String[] args) {
+
+        int l = 4;
+        int b = 5;
+        double p = (double) 2 * (l + b);
+
+        System.out.println(p);
+    }
+}
