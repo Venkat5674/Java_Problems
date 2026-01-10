81e00f9f43a6ad4628ba5cd790238aeb5844ef13
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2026-01-10 22:05:59 +0530

    Update LargestNumber.java

--- Collection_Framework/ArrayList/LargestNumber.java
+++ Collection_Framework/ArrayList/LargestNumber.java
@@ -5,7 +5,7 @@ class LargestNumber {
         Scanner sc = new Scanner(System.in);
 
         ArrayList<Integer> nums = new ArrayList<>();
-        System.out.println("Enter 5 numbers:");
+        System.out.println("Enter Five numbers:");
 
         for(int i = 0; i < 5; i++){
             nums.add(sc.nextInt());
